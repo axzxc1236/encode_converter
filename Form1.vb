71852bd7cf707a815185f2d942aad93e944da7f6
axzxc1236 <axzxc1236@gmail.com>
2014-03-16 15:39:41 +0800

    test

--- Form1.vb
+++ Form1.vb
@@ -3,23 +3,29 @@
 Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim ANSIString() As Byte, MyEncoder As New System.Text.ASCIIEncoding()
-        Dim fStream As IO.FileStream
-        Dim sFilename As String = Command()
-        Dim sSourceString As String = IO.File.ReadAllText(sFilename)
-        ANSIString = System.Text.Encoding.Convert(System.Text.Encoding.UTF8, System.Text.Encoding.ASCII, MyEncoder.GetBytes(sSourceString))
+        Select Case Command()
+            Case Is <> ""
+                Dim ANSIString() As Byte, MyEncoder As New System.Text.ASCIIEncoding()
+                Dim fStream As IO.FileStream
+                Dim sFilename As String = Command()
+                Dim sSourceString As String = IO.File.ReadAllText(sFilename)
+                ANSIString = System.Text.Encoding.Convert(System.Text.Encoding.UTF8, System.Text.Encoding.ASCII, MyEncoder.GetBytes(sSourceString))
 
-        fStream = File.OpenWrite(sFilename)
-        fStream.Write(ANSIString, 0, ANSIString.Length)
-        fStream.Close()
+                fStream = File.OpenWrite(sFilename)
+                fStream.Write(ANSIString, 0, ANSIString.Length)
+                fStream.Close()
 
-        Dim a As String = IO.File.ReadAllText(sFilename)
+                Dim a As String = IO.File.ReadAllText(sFilename)
 
-        a = a.Remove(a.Length - 3)
+                a = a.Remove(a.Length - 3)
 
-        IO.File.WriteAllText(sFilename, a)
+                IO.File.WriteAllText(sFilename, a)
 
-        End
+                End
+            Case Else
+                MsgBox("error", MsgBoxStyle.OkOnly)
+                End
+        End Select
     End Sub
    
 End Class
